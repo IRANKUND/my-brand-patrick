5b6e50a98c187db28d91bccbd0b1d141faf03b1b
unknown <irankundapatrick01@gmail.com>
2020-08-07 18:30:28 +0200

    add full authentication to the project

---
+++ script/auth.js
@@ -0,0 +1,23 @@
+
+const auth=firebase.auth();
+
+// listern for form
+
+document.getElementById('signupForm').addEventListener('submit', signup);
+
+function signup(e){
+  e.preventDefault();
+
+    var email=document.getElementById('email');
+    var password=document.getElementById('password');
+
+    const promise=firebase.auth().createUserWithEmailAndPassword(email.value, password.value); 
+        promise.catch(error => alert(error.message));
+        alert("signed Up");
+        document.getElementById('signupForm').reset();
+      
+
+}
+
+
+
